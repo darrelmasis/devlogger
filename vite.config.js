78cd925ba693f8a5c18b707bbadeaa1555e09819
Darel Masis <darrelmasis@gmail.com>
2025-11-22 14:53:12 -0600

    build: actualizar configuración de Vite
    - Configurar build para soportar iconos SVG\n- Optimizar configuración de bundling para la librería

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
   plugins: [
     react(),
-    cssInjectedByJsPlugin()
+    cssInjectedByJsPlugin(),
+    svgr()
   ],
   build: {
     lib: {
